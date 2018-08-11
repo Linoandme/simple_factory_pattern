9cbd5edf128cd7dc002e145d0bbd38d791e96e1a
lino <690102241@qq.com>
2018-08-12 02:07:25 +0800

    送耦合

--- src/Operation.java
+++ src/Operation.java
@@ -1,13 +1,25 @@
 public class Operation {
-    public static double GetResult(double num1,double num2,String operate){
-        double result = 0;
-        switch (operate){
-            case "+":result =num1+num2;break;
-            case "-":result=num1-num2;break;
-            case "/":result =num1/num2;break;
-            case "*":result=num1*num2;break;
+  private double num1;
+  private double num2;
 
-        }
+    public double getNum1() {
+        return num1;
+    }
+
+    public void setNum1(double num1) {
+        this.num1 = num1;
+    }
+
+    public double getNum2() {
+        return num2;
+    }
+
+    public void setNum2(double num2) {
+        this.num2 = num2;
+    }
+
+    public double GetResult(){
+        double result=0;
         return result;
     }
 }
